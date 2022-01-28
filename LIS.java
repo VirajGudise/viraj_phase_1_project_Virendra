f63c208204c1c1ef5dfcb933cde0a772119243d2
VirajGudise <97838176+VirajGudise@users.noreply.github.com>
2022-01-28 14:50:59 +0530

    Add files via upload

--- LIS.java
+++ LIS.java
@@ -1,47 +1,45 @@
-package assignments;
+package Ass.Projects;
 
- class LIS {
+class LIS {
+	static int max_ref; 
 
-		static int max_ref; 
+	static int _lis(int arr[], int n)
+	{
 
-		static int _lis(int arr[], int n)
+		if (n == 1)
+			return 1;
+
+		int res, max_ending_here = 1;
+
+		
+		for (int i = 1; i < n; i++) 
 		{
-
-			if (n == 1)
-				return 1;
-
-			int res, max_ending_here = 1;
-
-			
-			for (int i = 1; i < n; i++) 
-			{
-				res = _lis(arr, i);
-				if (arr[i - 1] < arr[n - 1]
-					&& res + 1 > max_ending_here)
-					max_ending_here = res + 1;
-			}
-
-			
-			if (max_ref < max_ending_here)
-				max_ref = max_ending_here;
-
-			
-			return max_ending_here;
+			res = _lis(arr, i);
+			if (arr[i - 1] < arr[n - 1]
+				&& res + 1 > max_ending_here)
+				max_ending_here = res + 1;
 		}
 
-		static int lis(int arr[], int n)
-		{
-			
-			max_ref = 1;
-			_lis(arr, n);
-			return max_ref;
-		}
+		
+		if (max_ref < max_ending_here)
+			max_ref = max_ending_here;
 
-		public static void main(String args[])
-		{
-			int arr[] = { 10, 22, 90, 33, 21, 50 };
-			int n = arr.length;
-			System.out.println("Length of lis is " + lis(arr, n)+ "\n");
-		}
+		
+		return max_ending_here;
 	}
 
+	static int lis(int arr[], int n)
+	{
+		
+		max_ref = 1;
+		_lis(arr, n);
+		return max_ref;
+	}
+
+	public static void main(String args[])
+	{
+		int arr[] = { 10, 22, 90, 33, 21, 50 };
+		int n = arr.length;
+		System.out.println("Length of lis is " + lis(arr, n)+ "\n");
+	}
+}
